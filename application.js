d6cb3e49fd8f7b1504c5ebbe9362c5f7e9269c3c
bitnami <bitnami@linux.(none)>
2012-06-03 19:55:52 +0000

    initial map changes - includes geocoding

---
+++ application.js
@@ -0,0 +1,45 @@
+var libpath = require('path'),
+    http = require("http"),
+    fs = require('fs'),
+    url = require("url");
+var path = ".";
+var port = 8088;
+
+http.createServer(function (request, response) {
+
+    var uri = url.parse(request.url).pathname;
+    var filename = libpath.join(path, uri);
+
+    libpath.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
+
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+           // var type = mime.lookup(filename);
+            response.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(port);
